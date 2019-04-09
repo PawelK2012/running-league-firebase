a24831eb49ca91961f96bd8cbeb3dbdf2ecabe45
Pawel Kaim <pawelkai@ie.ibm.com>
2019-04-09 20:34:03 +0100

    Skeleton for login comp and auth service

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+// import { auth } from 'firebase/app';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { User } from '../../models/User';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user$: Observable<User>;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
+    // this.user$ = this.afAuth.authState.switchMap(user => {
+    //   if (user) {
+    //     return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+    //   } else {
+    //     // return Observable.of(null);
+    //     return of(null);
+    //   }
+    // });
+  }
+
+  googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return this.oAuthLogin(provider);
+  }
+
+  private oAuthLogin(provider) {
+    return this.afAuth.auth.signInWithPopup(provider).then(credential => {
+      // update user documen in firestore
+      this.updateUserData(credential.user);
+    });
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
+  }
+
+  public updateUserData(user) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${user.user.uid}`
+    );
+    const data: User = {
+      uid: user.user.uid,
+      email: user.user.email,
+      roles: 'subscriber'
+      // roles: {
+      //   // subscriber: true
+      //   subscriber: true
+      // }
+    };
+    return userRef.set(data, { merge: true });
+  }
+
+  // canRead(user: User): boolean {
+  //   const allowed = ['admin', 'editor', 'subscriber'];
+  //   return this.checkAuthorization(user, allowed);
+  // }
+
+  // canEdit(user: User): boolean {
+  //   const allowed = ['admin', 'editor'];
+  //   return this.checkAuthorization(user, allowed);
+  // }
+
+  // canDelete(user: User): boolean {
+  //   const allowed = ['admin'];
+  //   return this.checkAuthorization(user, allowed);
+  // }
+
+  // private checkAuthorization(user: User, allowedRoles: string[]): boolean {
+  //   if (!user) {return false; }
+  //   for (const role of allowedRoles) {
+  //     if (user.roles[role]) {
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // }
+}
